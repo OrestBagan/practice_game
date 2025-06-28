1cc78a749bdf54d2e0ad8b5f9f96c2daef64232e
OrestBagan <orestbagan2233@gmail.com>
2025-06-29 00:01:52 +0300

    fixed problems

--- db.py
+++ db.py
@@ -2,13 +2,11 @@ import mysql.connector
 import requests
 
 def db():
-    def words_from_git():
-        url = "https://raw.githubusercontent.com/brown-uk/dict_uk/master/dict_uk.txt"
-        response = requests.get(url)
+    def words():
+        with open("words.txt", "r", encoding="utf-8") as file:
+            uk_words = file.read().splitlines()
+        return uk_words
 
-        uk_words = response.text.splitlines()
-
-        return uk_words[:20]
 
     conn = mysql.connector.connect(
         host="localhost",
@@ -19,27 +17,25 @@ def db():
 
     cursor = conn.cursor()
 
-    def table():
-        # Таблиця:
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS words (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                word VARCHAR(255) NOT NULL
-            )
-        """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS words (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            word VARCHAR(255) NOT NULL
+        )
+    """)
 
-        cursor.execute("SELECT COUNT(*) FROM words")
-        count = cursor.fetchone()[0]
+    cursor.execute("SELECT COUNT(*) FROM words")
+    count = cursor.fetchone()[0]
 
-        if count == 0:
-            cursor.executemany("INSERT INTO words (word) VALUES (%s)", [(w,) for w in words_from_git()])
-            conn.commit()
-            print("Слова додано у таблицю.")
-        else:
-            print("Таблиця вже містить дані. Нові слова не додано.")
+    if count == 0:
+        cursor.executemany("INSERT INTO words (word) VALUES (%s)", [(w,) for w in words()])
+        conn.commit()
+        print("Слова додано у таблицю.")
+    else:
+        print("Таблиця вже містить дані. Нові слова не додано.")
 
-        cursor.execute("SELECT * FROM words")
-        rows = cursor.fetchall()
+    cursor.execute("SELECT * FROM words")
+    rows = cursor.fetchall()
 
-        word_list = [row[1] for row in rows]
-        return word_list
+    word_list = [row[1] for row in rows]
+    return word_list
